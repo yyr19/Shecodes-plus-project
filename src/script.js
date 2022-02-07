753b48b8293cd7a055382e7f36bfe6fe670eb8e2
yyr19 <92030638+yyr19@users.noreply.github.com>
2022-02-07 15:10:21 +0100

    Created new repository

---
+++ src/script.js
@@ -0,0 +1,5 @@
+let apiKey = "2266db8caa8236c84abb00d76c94d010";
+let cityname = "Amsterdam";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}`;
+
+console.log(apiUrl);
