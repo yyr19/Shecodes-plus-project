15800cbf75b334976d6dd60b5f736333cf5698ed
yyr19 <92030638+yyr19@users.noreply.github.com>
2022-02-07 20:59:47 +0100

    added relevant icon

--- src/script.js
+++ src/script.js
@@ -32,6 +32,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#windspeed");
   let dateElement = document.querySelector("#current-date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -39,6 +40,11 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = response.data.wind.speed;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].main);
 }
 
 let apiKey = "2266db8caa8236c84abb00d76c94d010";
