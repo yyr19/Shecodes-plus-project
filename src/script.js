546a091d9b858c4a2f78e7ed50dc9420b7e92057
yyr19 <92030638+yyr19@users.noreply.github.com>
2022-02-07 21:32:21 +0100

    added the search engine

--- src/script.js
+++ src/script.js
@@ -38,7 +38,7 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.name;
   cloudElement.innerHTML = response.data.weather[0].main;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = response.data.wind.speed;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
@@ -47,10 +47,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].main);
 }
 
-let apiKey = "2266db8caa8236c84abb00d76c94d010";
-let cityname = "Amsterdam";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "2266db8caa8236c84abb00d76c94d010";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature, formatDate);
+}
 
-console.log(apiUrl);
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let currentCity = document.querySelector("#city");
+  currentCity.innerHTML = searchInput.value;
+  search(searchInput.value);
+}
 
-axios.get(apiUrl).then(displayTemperature, formatDate);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
