ee23c33bdeeeeae9e1ef2f5008769fddec7e0569
yyr19 <92030638+yyr19@users.noreply.github.com>
2022-02-07 16:36:17 +0100

    added temperature elements

--- src/script.js
+++ src/script.js
@@ -1,5 +1,21 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let cloudElement = document.querySelector("#clouds");
+  cloudElement.innerHTML = response.data.weather[0].main;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#windspeed");
+  windElement.innerHTML = response.data.wind.speed;
+}
+
 let apiKey = "2266db8caa8236c84abb00d76c94d010";
-let cityname = "Amsterdam";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}`;
+//let cityname = "Amsterdam";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
